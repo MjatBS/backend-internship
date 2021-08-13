9376895ee3cbd0b7e3c537b0ad0c798b47b7b3a7
Bogdan <bg.mjat@gmail.com>
2021-08-13 12:56:06 +0300

    fix bug

---
+++ tasks-week-2/TribonacciSequence.py
@@ -0,0 +1,27 @@
+def tribonacci(signature, n):
+    '''calculate Tribonacci series'''
+    if n < 0:
+        return []
+    else:
+        gen = anybonacci_gen(signature)
+        return [next(gen) for _ in range(n)]
+    
+
+def anybonacci_gen(signature):
+    '''Generate Anybonacci elements from the first position
+    Anybonacci sequence will created with any BASE [a, b, c, ..]
+    Example:
+        Fibonacci sequence have base [x, y]
+        Tribonacci sequence have base [x, y, z] 
+    '''
+    base = signature[:]
+    for now in base:
+        yield now
+    while True:
+        sum = 0
+        for i in range(len(base) - 1):
+            sum += base[i]
+            base[i] = base[i+1]
+        base[-1] += sum
+        now = base[-1]
+        yield now
